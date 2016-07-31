1706e64e010b3a93dfcb7f358b9a8221659d1d9e
saurabhs92 <saurabhs92@gmail.com>
2016-07-31 22:36:48 +0530

    Corrected urls related to posts by adding ending $.

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     #url(r'^', views.index, name='index'),
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^create/', views.post_create, name='post_create'),
-    url(r'^detail/', views.post_detail, name='post_detail'),
-    url(r'^update/', views.post_update, name='post_update'),
-    url(r'^delete/', views.post_delete, name='post_delete'),
+    url(r'^create/$', views.post_create, name='post_create'),
+    url(r'^detail/$', views.post_detail, name='post_detail'),
+    url(r'^update/$', views.post_update, name='post_update'),
+    url(r'^delete/$', views.post_delete, name='post_delete'),
 
 ]
