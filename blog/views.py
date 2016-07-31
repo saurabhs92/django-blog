0be6441bda34a343aa1a71e169cece0928bd4528
saurabhs92 <saurabhs92@gmail.com>
2016-07-31 22:32:57 +0530

    Added urls and dummy views to create, list, update, delete, view details of posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    data = {'indexdata': 'Data from views.py file.'}
-    return render(request, 'blog/index.html', data)
+#def index(request):
+#    data = {'indexdata': 'Data from views.py file.'}
+#    return render(request, 'blog/index.html', data)
+
+def post_list(request):
+    return HttpResponse('<h1>List</h1>')
+    
+def post_create(request):
+    return HttpResponse('<h1>Create</h1>')
+
+def post_detail(request):
+    return HttpResponse('<h1>Detail</h1>')
+
+def post_update(request):
+    return HttpResponse('<h1>Update</h1>')
+
+def post_delete(request):
+    return HttpResponse('<h1>Delete</h1>')
