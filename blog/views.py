cc450dc88281cdc387b24013558417eea491720f
saurabhs92 <saurabhs92@gmail.com>
2016-07-31 20:53:07 +0530

    Imported blog index html from a base template.

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world!!")
+    data = {'indexdata': 'Data from views.py file.'}
+    return render(request, 'blog/index.html', data)
