ee6d189e01851dfb093af2581a7450ea303f63f1
saurabhs92 <saurabhs92@gmail.com>
2016-07-31 22:13:08 +0530

    Added class PostModelAdmin to customize Post admin view.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Post, Author, Category, Tag
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'updated_date', 'author']
+    list_display_links = ['updated_date']
+    list_editable = ['title']
+    list_filter = ['updated_date', 'author']
+    search_fields = ['title', 'body']
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Tag)
