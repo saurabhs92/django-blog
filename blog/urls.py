e1a2b7d4728949f9641b6cb46ff173f527c1e50e
saurabhs92 <saurabhs92@gmail.com>
2016-08-02 03:06:11 +0530

    Added hyperlinks in post titles to view the detail post.

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     #url(r'^', views.index, name='index'),
     url(r'^$', views.post_list, name='post_list'),
     url(r'^create/$', views.post_create, name='post_create'),
-    url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<id>\d+)/$', views.post_detail, name='detail'),
     url(r'^update/$', views.post_update, name='post_update'),
     url(r'^delete/$', views.post_delete, name='post_delete'),
 
