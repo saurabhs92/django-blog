2677fa4032dbe621c42c8866ba7c531825d783d1
saurabhs92 <saurabhs92@gmail.com>
2016-08-02 02:28:50 +0530

    blog home now lists all posts, detail page opens up post with id=2.

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 #def index(request):
 #    data = {'indexdata': 'Data from views.py file.'}
 #    return render(request, 'blog/index.html', data)
 
 def post_list(request):
-    return render(request, 'base.html', {})
+    queryset = Post.objects.all()
+    context = {
+        'object_list': queryset,
+        'title': 'Posts'
+    }
+    return render(request, 'blog/index.html', context)
+
+def post_detail(request):
+    instance = get_object_or_404(Post, id=2)
+    context = {
+        'title': instance.title,
+        'instance': instance
+    }
+    return render(request, 'blog/post_detail.html', context)
     
 def post_create(request):
     return HttpResponse('<h1>Create</h1>')
-
-def post_detail(request):
-    return HttpResponse('<h1>Detail</h1>')
 
 def post_update(request):
     return HttpResponse('<h1>Update</h1>')
