90de2dfdabd6d1971c327921d0aa3903fffc3302
saurabhs92 <saurabhs92@gmail.com>
2016-08-10 21:31:49 +0530

    Added pagination using paginator.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
@@ -9,7 +10,19 @@ from .forms import PostForm
 #    return render(request, 'blog/index.html', data)
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 5) # Show 5 entries per page
+    
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         'object_list': queryset,
         'title': 'Posts'
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/home/saurabh/work/django/blog-django/venv/bin/python
 import os
 import sys
 
