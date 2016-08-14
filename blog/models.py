f2955fee9182cdf602eccb94edb5edaae1928b4b
saurabhs92 <saurabhs92@gmail.com>
2016-08-15 05:26:16 +0530

    Displayed images in post list and post detail urls

--- blog/models.py
+++ blog/models.py
@@ -34,7 +34,9 @@ class Post(models.Model):
     Defines the blog_post table
     """
     title = models.CharField(max_length=200)
-    image = models.FileField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, height_field='height_field', width_field='width_field')
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
     body  = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_date = models.DateTimeField(auto_now_add=False, auto_now=True)
