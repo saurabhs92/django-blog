8c33c63eb1c2c7dd3e3d093e84ab6afb24f46bfc
saurabhs92 <saurabhs92@gmail.com>
2016-08-23 23:42:26 +0530

    Staff, superuser permission checks for create, update and delete posts pages

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from urllib.parse import quote_plus
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
 from .models import Post
@@ -42,6 +42,8 @@ def post_detail(request, slug):
     return render(request, 'blog/post_detail.html', context)
     
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -55,6 +57,8 @@ def post_create(request):
     return render(request, 'blog/post_create.html', context)
 
 def post_update(request, slug):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -70,6 +74,8 @@ def post_update(request, slug):
     return render(request, 'blog/post_create.html', context)
 
 def post_delete(request, slug):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, 'Successfully deleted.')
