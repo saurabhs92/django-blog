9e5859d26afb3c23984c365630c95c85acbd2ec8
saurabhs92 <saurabhs92@gmail.com>
2016-09-02 01:06:37 +0530

    Added generic content types instead on post field in comments.

--- comments/models.py
+++ comments/models.py
@@ -1,9 +1,16 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from blog.models import Post, Author
 
 class Comment(models.Model):
     user 	= models.ForeignKey(Author)
-    post 	= models.ForeignKey(Post)
+    # post 	= models.ForeignKey(Post)
+    
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
     content 	= models.TextField()
     timestamp 	= models.DateTimeField(auto_now_add=True, auto_now=False)
 
