e0c9661c90bc478e4b85cd94dd09eb8e514cf5b2
saurabhs92 <saurabhs92@gmail.com>
2016-09-08 18:57:09 +0530

    Removed unwanted import and comments queryset display.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from urllib.parse import quote_plus
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from django.contrib import messages
